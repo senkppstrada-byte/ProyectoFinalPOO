25db81e939b38415fe050265df19ca72572a0298
JR <josericardomartinez@polimercedes.edu.do>
2026-07-21 11:50:10 -0400

    Maquetacion de clase Bolsa y declaracion de paquete visual
    (gets, sets, constructor formato Singleton)

--- ProyectoFinal/src/logico/Bolsa.java
+++ ProyectoFinal/src/logico/Bolsa.java
@@ -1,5 +1,60 @@
 package logico;
 
+import java.util.ArrayList;
 public class Bolsa {
-
+	
+	private ArrayList<Empresa> lasEmpresas;
+	private ArrayList<Persona> lasPersonas;
+	private ArrayList<Oferta> lasOfertas;
+	private ArrayList<Solicitud> lasSolicitudes;
+	private static Bolsa bolsa = null;
+	
+	private Bolsa() {
+		lasEmpresas = new ArrayList<>();
+		lasPersonas = new ArrayList<>();
+		lasOfertas = new ArrayList<>();
+		lasSolicitudes = new ArrayList<>();
+	}
+	
+	public static Bolsa getInstancia() {
+		if(bolsa == null) {
+			bolsa = new Bolsa();
+		}
+		return bolsa;
+	}
+	
+	public ArrayList<Persona> conectarCandidatos(Oferta of) {
+		ArrayList<Persona> cands;
+		return cands;
+ 	}
+	public ArrayList<Empresa> getLasEmpresas() {
+		return lasEmpresas;
+	}
+	public void setLasEmpresas(ArrayList<Empresa> lasEmpresas) {
+		this.lasEmpresas = lasEmpresas;
+	}
+	public ArrayList<Persona> getLasPersonas() {
+		return lasPersonas;
+	}
+	public void setLasPersonas(ArrayList<Persona> lasPersonas) {
+		this.lasPersonas = lasPersonas;
+	}
+	public ArrayList<Oferta> getLasOfertas() {
+		return lasOfertas;
+	}
+	public void setLasOfertas(ArrayList<Oferta> lasOfertas) {
+		this.lasOfertas = lasOfertas;
+	}
+	public ArrayList<Solicitud> getLasSolicitudes() {
+		return lasSolicitudes;
+	}
+	public void setLasSolicitudes(ArrayList<Solicitud> lasSolicitudes) {
+		this.lasSolicitudes = lasSolicitudes;
+	}
+	
+	
+	
+	
+	
+	
 }

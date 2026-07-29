196e3172cc4165e831f4e268dbbb5f37d06afe96
lenov <lenov@LAPTOP-RMCU61UM>
2026-07-29 09:59:50 -0400

    Interfazde la pantalla login

--- ProyectoFinal/src/visual/Login.java
+++ ProyectoFinal/src/visual/Login.java
@@ -26,4 +26,48 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtUsuario;
 	private JPasswordField txtClave;
+	
+	public Login() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Bolsa de Trabajo - Ingreso");
+        setBounds(100, 100, 450, 300);
+        setLocationRelativeTo(null);
+
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        JPanel panel = new JPanel();
+        contentPane.add(panel, BorderLayout.CENTER);
+        panel.setLayout(null);
+
+        JLabel lblUsuario = new JLabel("Usuario:");
+        lblUsuario.setBounds(39, 39, 105, 14);
+        panel.add(lblUsuario);
+
+        txtUsuario = new JTextField();
+        txtUsuario.setBounds(39, 64, 191, 20);
+        panel.add(txtUsuario);
+        txtUsuario.setColumns(10);
+
+        JLabel lblClave = new JLabel("Clave:");
+        lblClave.setBounds(39, 98, 105, 14);
+        panel.add(lblClave);
+
+        txtClave = new JPasswordField();
+        txtClave.setBounds(39, 128, 191, 20);
+        panel.add(txtClave);
+
+        JButton btnLogin = new JButton("Ingresar");
+        btnLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ingresar();
+            }
+        });
+        btnLogin.setBounds(39, 175, 105, 23);
+        panel.add(btnLogin);
+
+        getRootPane().setDefaultButton(btnLogin);
+    }
 }

455b22953e4925bb17e238a7003c7f528baec2c7
lenov <lenov@LAPTOP-RMCU61UM>
2026-07-29 09:47:58 -0400

    Clase Login, pantalla de ingreso

---
+++ ProyectoFinal/src/visual/Login.java
@@ -0,0 +1,29 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import logica.BolsaLaboral;
+import logica.Candidato;
+import logica.CentroEmpleador;
+import logica.CuentaUsuario;
+import logica.Representante;
+import logica.Tecnico;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtClave;
+}

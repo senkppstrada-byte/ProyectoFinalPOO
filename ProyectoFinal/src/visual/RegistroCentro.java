f5ddbaa063f8c862b3ea74b3262a5ff69eb4c930
lenov <lenov@192.168.56.1>
2026-07-31 13:33:55 -0400

    Validar centro duplicado

--- ProyectoFinal/src/visual/RegistroCentro.java
+++ ProyectoFinal/src/visual/RegistroCentro.java
@@ -142,6 +142,18 @@ public class RegistroCentro extends JDialog {
 	}
 
 	private boolean usuarioExiste(String usuario) {
+		BolsaLaboral bolsa = BolsaLaboral.getInstancia();
+		for (Candidato c : bolsa.getCandidatos()) {
+			if (c.getCuenta() != null && c.getCuenta().getNombreUsuario().equalsIgnoreCase(usuario)) {
+				return true;
+			}
+		}
+		for (CentroEmpleador ce : bolsa.getCentros()) {
+			if (ce.getRep() != null && ce.getRep().getCuenta() != null
+					&& ce.getRep().getCuenta().getNombreUsuario().equalsIgnoreCase(usuario)) {
+				return true;
+			}
+		}
 		return false;
 	}
 }
